d554555f26171811a9f43b1cd132e6db54b79697
Michel Machado <michelmachado125@gmail.com>
2024-03-20 13:46:46 -0300

    primeiro commit removida config de segurança e conectado ao banco postgres

--- src/main/java/com/grupo04pj01/urna/UrnaApplication.java
+++ src/main/java/com/grupo04pj01/urna/UrnaApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.grupo04pj01.urna"})
 public class UrnaApplication {
 
 	public static void main(String[] args) {
